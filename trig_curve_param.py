bfd7c0273efecab8956753d942d5ba2485c64e57
Christian Schwerdt <christianjosef@hotmail.com>
2023-12-28 10:22:10 +0100

    Added center parameter to sin curve parameter visualization and also added display of sine curve equation

--- trig_curve_param.py
+++ trig_curve_param.py
@@ -4,16 +4,17 @@ import numpy as np
 
 app = Dash(__name__)
 
-app.layout = html.Div([html.H4('Sinus curve parameters visualization'), dcc.Graph(id="sin_curve"), html.P(id = "area_calculations"), html.Button("Dec phase", n_clicks=0, id='btn-decPhase'), html.Button("Inc phase", n_clicks=0, id='btn-incPhase'), html.Button("Dec Amp", n_clicks=0, id='btn-decAmp'), html.Button("Inc Amp", n_clicks=0, id='btn-incAmp'), html.Button("Dec Freq", n_clicks=0, id='btn-decFreq'), html.Button("Inc Freq", n_clicks=0, id='btn-incFreq')])
+app.layout = html.Div([html.H4('Sinus curve parameters visualization'), dcc.Graph(id="sin_curve"), html.P(id = "sin_parameters"), html.P(id = "sin_equation"), html.Button("Dec Amp", n_clicks=0, id='btn-decAmp'), html.Button("Inc Amp", n_clicks=0, id='btn-incAmp'), html.Button("Dec Freq", n_clicks=0, id='btn-decFreq'), html.Button("Inc Freq", n_clicks=0, id='btn-incFreq'), html.Button("Dec phase", n_clicks=0, id='btn-decPhase'), html.Button("Inc phase", n_clicks=0, id='btn-incPhase'), html.Button("Dec Center", n_clicks=0, id='btn-decCenter'), html.Button("Inc Center", n_clicks=0, id='btn-incCenter')])
 
 
-@app.callback(Output("sin_curve", "figure"), Input("btn-decPhase", "n_clicks"), Input("btn-incPhase", "n_clicks"), Input("btn-decAmp", "n_clicks"), Input("btn-incAmp", "n_clicks"), Input("btn-decFreq", "n_clicks"), Input("btn-incFreq", "n_clicks"))
-def draw_sin_curve(n_decPhase, n_incPhase, n_decAmp, n_incAmp, n_decFreq, n_incFreq):
+@app.callback(Output("sin_curve", "figure"), Input("btn-decPhase", "n_clicks"), Input("btn-incPhase", "n_clicks"), Input("btn-decAmp", "n_clicks"), Input("btn-incAmp", "n_clicks"), Input("btn-decFreq", "n_clicks"), Input("btn-incFreq", "n_clicks"), Input("btn-decCenter", "n_clicks"), Input("btn-incCenter", "n_clicks"))
+def draw_sin_curve(n_decPhase, n_incPhase, n_decAmp, n_incAmp, n_decFreq, n_incFreq, n_decCenter, n_incCenter):
     n_phase = 0.1 * (n_incPhase - n_decPhase)
     n_amp = 1 + (n_incAmp - n_decAmp)
     n_freq = 1 + (n_incFreq - n_decFreq)
+    n_center = (n_incCenter - n_decCenter)
     x = np.linspace(-2*np.pi, 2*np.pi, 100)
-    y = n_amp * np.sin(n_freq*x + n_phase)
+    y = n_amp * np.sin(n_freq*x + n_phase) + n_center
 
     trace1 = go.Scatter(
         x=x,
@@ -29,12 +30,20 @@ def draw_sin_curve(n_decPhase, n_incPhase, n_decAmp, n_incAmp, n_decFreq, n_incF
     return fig
 
 
-@app.callback(Output("area_calculations", "children"), Input("btn-decPhase", "n_clicks"), Input("btn-incPhase", "n_clicks"), Input("btn-decAmp", "n_clicks"), Input("btn-incAmp", "n_clicks"), Input("btn-decFreq", "n_clicks"), Input("btn-incFreq", "n_clicks"))
-def area_formula(n_decPhase, n_incPhase, n_decAmp, n_incAmp, n_decFreq, n_incFreq):
+@app.callback(Output("sin_parameters", "children"), Input("btn-decPhase", "n_clicks"), Input("btn-incPhase", "n_clicks"), Input("btn-decAmp", "n_clicks"), Input("btn-incAmp", "n_clicks"), Input("btn-decFreq", "n_clicks"), Input("btn-incFreq", "n_clicks"), Input("btn-decCenter", "n_clicks"), Input("btn-incCenter", "n_clicks"))
+def area_formula(n_decPhase, n_incPhase, n_decAmp, n_incAmp, n_decFreq, n_incFreq, n_decCenter, n_incCenter):
     n_phase = 0.1 * (n_incPhase - n_decPhase)
     n_amp = 1 + (n_incAmp - n_decAmp)
     n_freq = 1 + (n_incFreq - n_decFreq)
-    return f"Phase: {n_phase} and Amplitude: {n_amp} and Frequency: {n_freq}" 
+    n_center = (n_incCenter - n_decCenter)
+    return f"Phase: {n_phase} and Amplitude: {n_amp} and Frequency: {n_freq} and Center: {n_center}" 
 
+@app.callback(Output("sin_equation", "children"), Input("btn-decPhase", "n_clicks"), Input("btn-incPhase", "n_clicks"), Input("btn-decAmp", "n_clicks"), Input("btn-incAmp", "n_clicks"), Input("btn-decFreq", "n_clicks"), Input("btn-incFreq", "n_clicks"), Input("btn-decCenter", "n_clicks"), Input("btn-incCenter", "n_clicks"))
+def area_formula(n_decPhase, n_incPhase, n_decAmp, n_incAmp, n_decFreq, n_incFreq, n_decCenter, n_incCenter):
+    n_phase = 0.1 * (n_incPhase - n_decPhase)
+    n_amp = 1 + (n_incAmp - n_decAmp)
+    n_freq = 1 + (n_incFreq - n_decFreq)
+    n_center = (n_incCenter - n_decCenter)
+    return f"y = {n_amp}* sin({n_freq} * x + {n_phase}) + {n_center}"
 
 app.run_server(debug=True)
